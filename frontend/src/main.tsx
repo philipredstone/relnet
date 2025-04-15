331cae67207918e8e282b2bb2b19231545492ad5
Tobias Hopp <tobi@gaminggeneration.de>
2025-04-15 16:44:23 +0200

    Fix tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './app.css';
 
 // Create root and render the App component into the HTML element with ID 'root'
 const rootElement = document.getElementById('root');
